16db51de901369b9c1a8da8fa10895a020d3d97b
Ian Fonberg <fonbergi@oregonstate.edu>
2021-06-02 13:49:42 -0700

    Add sign validation.

--- Proj6_fonbergi.asm
+++ Proj6_fonbergi.asm
@@ -1,4 +1,4 @@
-TITLE Program Template     (Proj5_fonbergi.asm)
+TITLE Program Template     (Proj6_fonbergi.asm)
 
 ; Author: Ian Fonberg
 ; Last Modified: 5/31/2021
@@ -43,7 +43,7 @@ mGetString MACRO getPrompt:REQ, getLength:REQ, getDest:REQ, getBytes:REQ
 ; Store Raw Input
     MOV     ECX, getLength      ; Max limit of readable characters.
     MOV     EDX, getDest
-    
+
     CALL    ReadString
 
     MOV     EDI, getBytes
@@ -85,7 +85,10 @@ MAX_INPUT = 12
 
 .data
 
-firstAttempt    BYTE    "Please enter an signed number: ",0
+firstAttempt    BYTE    "Please enter a signed number: ",0
+secondAttempt   BYTE    "Please try again: ",0
+emptyMsg        BYTE    "ERROR: a value is required.",13,10,0
+errorMsg        BYTE    "ERROR: You did not enter a signed number or your number was too big.",13,10,0
 rawNumString    BYTE    MAX_INPUT DUP(0)
 bytesRead       DWORD   ?
 validNum        DWORD   ?
@@ -94,6 +97,9 @@ validNum        DWORD   ?
 main PROC
 
     PUSH    OFFSET firstAttempt
+    PUSH    OFFSET emptyMsg
+    PUSH    OFFSET errorMsg
+    PUSH    OFFSET secondAttempt
     PUSH    OFFSET rawNumString
     PUSH    OFFSET bytesRead
     PUSH    validNum
@@ -105,6 +111,9 @@ main PROC
     CALL    WriteDec
 
     PUSH    OFFSET firstAttempt
+    PUSH    OFFSET emptyMsg
+    PUSH    OFFSET errorMsg
+    PUSH    OFFSET secondAttempt
     PUSH    OFFSET rawNumString
     PUSH    OFFSET bytesRead
     PUSH    validNum
@@ -132,21 +141,72 @@ main ENDP
 ; Postconditions: none
 ;
 ; Receives:
-;   [EBP+20]    = input, offset of first attemnpt prompt
+;   [EBP+32]    = input, offset of first attempt prompt
+;   [EBP+28]    = input, offset of empty error message
+;   [EBP+24]    = input, offset of invalid error message
+;   [EBP+20]    = input, offset of second attempt prompt
 ;   [EBP+16]    = output, offset of user input string
 ;   [EBP+12]    = output, offset of number of bytes read
 ;   [EBP+8]     = output, offset of number storage
 ;
 ; Returns: [EBP+8] = validated number
 ; ---------------------------------------------------------------------------------
-ReadVal PROC
-    PUSH    EBP
-    MOV     EBP, ESP
+ReadVal PROC USES EAX ECX EDX ESI
+    LOCAL valid:BYTE, numInt:DWORD, sign:DWORD
+    
+    MOV     valid, 1                                    ; Initialize valid to true.
+; Display initial prompt for signed number.
+    mGetString [EBP+32], MAX_INPUT, [EBP+16], [EBP+12]
 
+_validateDigits:
+    MOV     numInt, 0                                   ; Initialize integer aggregator to 0.
+    MOV     sign, 1                                     ; Value is unsigned.
+
+    CMP     valid, 0
+    JNE     _endTryAgain
+
+; Prompt for new signed integer.
     mGetString [EBP+20], MAX_INPUT, [EBP+16], [EBP+12]
 
-    POP     EBP
-    RET
+    MOV     ECX, bytesRead
+
+_endTryAgain:
+; Assert a value was read.
+    CMP     bytesRead, 0
+    JNE     _notEmpty
+
+; Set error state.
+    MOV     valid, 0                                    ; Valid is false.
+    MOV     EDX, [EBP+28]
+    CALL    WriteString
+    JMP     _validateDigits
+_notEmpty:
+    MOV     ESI, [EBP+16]                               ; Move user input to ESI.
+    
+    CLD
+    LODSB                                               ; Load first character
+
+    CMP     AL, 45                                      ; is the character a '-' sign?
+    JE      _setNegative
+    CMP     AL, 43                                      ; is the character a '+' sign?
+    JE      _checkLength
+    JMP     _aggregateNum                               ; Proceed with calculation.
+
+_setNegative:
+    MOV     sign, -1
+_checkLength:
+    CMP     bytesRead, 1                                ; Assert sign is not the only component.
+    JNE     _loadNext
+; Set error state.
+    MOV     valid, 0                                    ; Valid is false.
+    MOV     EDX, [EBP+24]
+    CALL    WriteString
+    JMP     _validateDigits
+_loadNext:
+    LODSB
+_aggregateNum:
+
+    RET 24
 ReadVal ENDP
 
 
